8e5cfdf3a5a91658ef3f367844582dae352e4b65
Roei Sabag <Sabag@IL-NUA-PW0GREFF.(none)>
2025-08-17 10:49:07 +0300

    Fix includes to use include/ and add proper install rules

--- ports/hello/portfile.cmake
+++ ports/hello/portfile.cmake
@@ -1,6 +1,15 @@
-vcpkg_configure_cmake(
-  SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR}
+set(SOURCE_PATH "${CURRENT_PORT_DIR}")
+
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
   PREFER_NINJA
 )
-vcpkg_install_cmake()
-vcpkg_copy_pdbs()  # (Optional, handles Windows PDB files if any)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/hello PACKAGE_NAME hello)
+
+# Optional usage file & license
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage"
+"find_package(hello CONFIG REQUIRED)\n"
+"target_link_libraries(yourapp PRIVATE hello::hello)\n")
+vcpkg_install_copyright(FILE_LIST "${CURRENT_PORT_DIR}/LICENSE")
